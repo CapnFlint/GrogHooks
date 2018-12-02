0a67ccdab972bb91624e3a68677b6ae84fbba1ad
Capn Flint <capnflinttv@gmail.com>
2018-12-01 23:22:20 -0800

    new test....

--- Src/hooks/hook_helper.py
+++ Src/hooks/hook_helper.py
@@ -1,7 +1,7 @@
 hook_register = dict()
 hook_headers = dict()
 
-def register_hook(path):
+def _register_hook(path):
     global hook_register
     def wrap(hook):
         print "Registering Hook: " + path
@@ -21,3 +21,30 @@ def set_headers(headers):
         hook_headers[func] = headers
         return wrapped
     return wrap
+
+def register_hook(path):
+    global hook_register
+    print "Registering Hook: " + path
+    class NewCls(hook):
+        def __init__(self,*args,**kwargs):
+            self.oInstance = Cls(*args,**kwargs)
+        def __getattribute__(self,s):
+            """
+            this is called whenever any attribute of a NewCls object is accessed. This function first tries to
+            get the attribute off NewCls. If it fails then it tries to fetch the attribute from self.oInstance (an
+            instance of the decorated class). If it manages to fetch the attribute from self.oInstance, and
+            the attribute is an instance method then `time_this` is applied.
+            """
+            try:
+                x = super(NewCls,self).__getattribute__(s)
+            except AttributeError:
+                pass
+            else:
+                return x
+            x = self.oInstance.__getattribute__(s)
+            if type(x) == type(self.__init__): # it is an instance method
+                return time_this(x)                 # this is equivalent of just decorating the method with time_this
+            else:
+                return x
+    hook_register[path] = NewCls
+    return NewCls
