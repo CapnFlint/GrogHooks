606b3aa7f635d6155baeee5359790a103053c815
Capn Flint <capnflinttv@gmail.com>
2018-12-01 22:01:56 -0800

    I'm an idiot... lol

--- Src/groghooks.py
+++ Src/groghooks.py
@@ -53,7 +53,7 @@ class MyHandler(BaseHTTPRequestHandler):
             query = parse_qs(query)
         try:
             if path in config.available_hooks and query:
-                sendResponse(self, 200, {}, hub.challenge)
+                sendResponse(self, 200, {}, query['hub.challenge'])
                 return
             else:
                 send404(self, path)
