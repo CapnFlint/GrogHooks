511e744dcb2ae8f31d3447640ab56d900b785fec
Capn Flint <capnflinttv@gmail.com>
2018-12-01 23:25:17 -0800

    the wrap is needed when using parameters

--- Src/hooks/hook_helper.py
+++ Src/hooks/hook_helper.py
@@ -23,28 +23,30 @@ def set_headers(headers):
     return wrap
 
 def register_hook(path):
-    global hook_register
-    print "Registering Hook: " + path
-    class NewCls(object):
-        def __init__(self,*args,**kwargs):
-            self.oInstance = Cls(*args,**kwargs)
-        def __getattribute__(self,s):
-            """
-            this is called whenever any attribute of a NewCls object is accessed. This function first tries to
-            get the attribute off NewCls. If it fails then it tries to fetch the attribute from self.oInstance (an
-            instance of the decorated class). If it manages to fetch the attribute from self.oInstance, and
-            the attribute is an instance method then `time_this` is applied.
-            """
-            try:
-                x = super(NewCls,self).__getattribute__(s)
-            except AttributeError:
-                pass
-            else:
-                return x
-            x = self.oInstance.__getattribute__(s)
-            if type(x) == type(self.__init__): # it is an instance method
-                return time_this(x)                 # this is equivalent of just decorating the method with time_this
-            else:
-                return x
-    hook_register[path] = NewCls
-    return NewCls
+    def wrap(hook):
+        global hook_register
+        print "Registering Hook: " + path
+        class NewCls(object):
+            def __init__(self,*args,**kwargs):
+                self.oInstance = hook(*args,**kwargs)
+            def __getattribute__(self,s):
+                """
+                this is called whenever any attribute of a NewCls object is accessed. This function first tries to
+                get the attribute off NewCls. If it fails then it tries to fetch the attribute from self.oInstance (an
+                instance of the decorated class). If it manages to fetch the attribute from self.oInstance, and
+                the attribute is an instance method then `time_this` is applied.
+                """
+                try:
+                    x = super(NewCls,self).__getattribute__(s)
+                except AttributeError:
+                    pass
+                else:
+                    return x
+                x = self.oInstance.__getattribute__(s)
+                if type(x) == type(self.__init__): # it is an instance method
+                    return time_this(x)                 # this is equivalent of just decorating the method with time_this
+                else:
+                    return x
+        hook_register[path] = NewCls
+        return NewCls
+    return wrap
